d6f4679b2ebd0e25e0b23d7c928d323e47c1d1ab
ratseater <50141936+ratseater@users.noreply.github.com>
2021-06-10 15:00:47 +0700

    Update client.py

--- flaskapp/client.py
+++ flaskapp/client.py
@@ -1,4 +1,3 @@
-'''
 import requests
 r = requests.get('http://localhost:5000/')
 print(r.status_code)
@@ -6,7 +5,6 @@ print(r.text)
 r = requests.get('http://localhost:5000/data_to') 
 print(r.status_code)
 print(r.text)
-'''
 
 # импортируем нужные модули
 import os
