58b971132b7d2a9219ae64da62606d2bec596036
ratseater <50141936+ratseater@users.noreply.github.com>
2021-06-10 19:09:31 +0700

    Update client.py

--- flaskapp/client.py
+++ flaskapp/client.py
@@ -25,4 +25,13 @@ jsondata = {'imagebin':b64.decode('utf-8')}
 res = requests.post('http://localhost:5000/apinet', json=jsondata)
 if res.ok:
  print(res.json()) 
- 
+
+# Тестируем xml шаблоны и ловим исключения
+try:
+ r = requests.get('http://localhost:5000/apixml')
+ print(r.status_code)
+ if(r.status_code!=200):
+  exit(1)
+ print(r.text)
+except:
+ exit(1) 
