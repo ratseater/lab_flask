0d99ff47e2a72e23152037b650d180a1ad3607c5
ratseater <50141936+ratseater@users.noreply.github.com>
2021-06-15 16:48:13 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -1,3 +1,17 @@
+from flask import render_template
+
+#наша новая функция сайта
+@app.route("/data_to")
+def data_to():
+    #создаем переменные с данными для передачи в шаблон
+    some_pars = {'user':'Ivan','color':'red'}
+    some_str ='Hello my dear friends!'
+    some_value = 10
+    #передаем данные в шаблон и вызываем его
+    return render_template('simple.html',some_str = some_str,
+
+some_value = some_value,some_pars=some_pars)
+
 # модули работы с формами и полями в формах
 from flask_wtf import FlaskForm,RecaptchaField
 from wtforms import StringField, SubmitField, TextAreaField
