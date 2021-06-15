5cd201e18208f65df8cac5ac51367a9b5c0068d4
ratseater <50141936+ratseater@users.noreply.github.com>
2021-06-15 16:40:56 +0700

    Update client.py

--- flaskapp/client.py
+++ flaskapp/client.py
@@ -1,37 +1,32 @@
-import requests
-r = requests.get('http://localhost:5000/')
-print(r.status_code)
-print(r.text)
-r = requests.get('http://localhost:5000/data_to') 
-print(r.status_code)
-print(r.text)
-
 # импортируем нужные модули
 import os
-from io import BytesIO 
-88
-import base64
+from io import BytesIO
+import base
+
 img_data = None
 # создаем путь к файлу (для кроссплатформенности, например)
 path = os.path.join('./static','image0008.png')
-# читаем файл и енкодируем его в строку base64
-with open(path, 'rb') as fh:
- img_data = fh.read()
- b64 = base64.b64encode(img_data)
+
+
+# читаем файл и енкодируем его в строку base
+with open(path,'rb') as fh:
+    img_data = fh.read()
+
+b64 = base64.b64encode(img_data)
+
 # создаем json словарь, который
 # отправляется на сервер в виде json строки
 # преобразование делает сама функция отправки запроса post
 jsondata = {'imagebin':b64.decode('utf-8')}
 res = requests.post('http://localhost:5000/apinet', json=jsondata)
 if res.ok:
- print(res.json()) 
+    print(res.json())
 
-# Тестируем xml шаблоны и ловим исключения
 try:
- r = requests.get('http://localhost:5000/apixml')
- print(r.status_code)
- if(r.status_code!=200):
-  exit(1)
- print(r.text)
+    r = requests.get('http://localhost:5000/apixml')
+    print(r.status_code)
+    if(r.status_code!=200):
+        exit(1)
+    print(r.text)
 except:
- exit(1) 
+    exit(1)
